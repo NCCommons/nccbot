b4506c59a3e0d7f414d1a10c2dfa232279264650
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-08 05:36:24 +0300

    Update log directory paths to use HOME environment variable
    Changed directory path initialization to use the HOME environment variable if available, falling back to a default path otherwise. This improves portability and allows the code to adapt to different user environments.

--- logs_fix/files.py
+++ logs_fix/files.py
@@ -2,8 +2,11 @@
 from logs_fix.files import has_url_dir, move_text_dir
 """
 from pathlib import Path
+import os
 
-Dir = Path(__file__).parent
+home_dir = os.getenv("HOME")
+project = home_dir if home_dir else "I:/ncc"
+logs_fix_dir = Path(project) / "ncc_data/logs_fix"
 
-has_url_dir = Dir / "has_url"
-move_text_dir = Dir / "move_text"
+has_url_dir = logs_fix_dir / "has_url"
+move_text_dir = logs_fix_dir / "move_text"
